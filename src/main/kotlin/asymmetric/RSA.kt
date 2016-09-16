80a8ee4e24899176c2899b355f7171d33d52f1fd
breandan.considine <breandan.considine@gmail.com>
2016-09-16 02:10:14 -0400

    Add modInverse exercise

--- src/main/kotlin/asymmetric/RSA.kt
+++ src/main/kotlin/asymmetric/RSA.kt
@@ -4,9 +4,10 @@ import java.math.BigInteger
 import java.security.SecureRandom
 import java.util.*
 
+private val BITS = 14
 private val r = SecureRandom()
-private val p = BigInteger.probablePrime(1024, r)
-private val q = BigInteger.probablePrime(1024, r)
+private val p = BigInteger.probablePrime(BITS, r)
+private val q = BigInteger.probablePrime(BITS, r)
 private val φ = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE))
 
 public val pq = p.multiply(q)
@@ -17,29 +18,40 @@ fun main(args: Array<String>) {
     e = e.add(BigInteger.valueOf(2))
 
   println("Public key: " + pq)
-  val s = encrypt("this is a very difficult message to brute force")
+  val s = encrypt("yes")
   println("Ciphertext: " + s)
 
   val d = decrypt(s)
   println("Decrypted:  " + d)
 }
 
-internal fun encrypt(plaintext: String): String {
-  return String(
-    Base64
-      .getEncoder()
-      .encode(
-        BigInteger(plaintext.toByteArray())
-          .modPow(e, pq)
-          .toByteArray()))
+private fun encrypt(plaintext: String): String {
+  val m = plaintext.toByteArray()
+  // c = M^e (mod pq)
+  val c = BigInteger(m).modPow(e, pq)
+  return Base64.getEncoder().encodeToString(c.toByteArray())
 }
 
-internal fun decrypt(ciphertext: String): String {
-  return String(
-    BigInteger(
-      Base64
-        .getDecoder()
-        .decode(ciphertext))
-      .modPow(e.modInverse(φ), pq)
-      .toByteArray())
+private fun decrypt(ciphertext: String): String {
+  val c = Base64.getDecoder().decode(ciphertext)
+  // ed = 1 (mod φ)
+  // val d = e.modInverse(φ)
+  val d = BigInteger.valueOf(modInverse(e.toLong(), φ.toLong()))
+  println(d)
+  // C^d (mod pq)
+  val p = BigInteger(c).modPow(d, pq)
+  return String(p.toByteArray())
+}
+
+/**
+ * Returns a number x, s.t. ax = 1 (mod m)
+ */
+
+private fun modInverse(a: Long, m: Long): Long {
+  var x = 1L
+  var one = 1L
+
+  // Your code goes here
+
+  return x
 }
