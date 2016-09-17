ceaf9217f3cea072603f8e7355b3a9e654f45655
Breandan Considine <breandan.considine@gmail.com>
2016-09-17 09:41:29 -0400

    Time to factor

--- src/main/kotlin/exercises/PrimeExercise.kt
+++ src/main/kotlin/exercises/PrimeExercise.kt
@@ -1,9 +1,20 @@
 package exercises
 
+import java.math.BigInteger
+import java.util.*
+
 private fun isPrime(number: Int): Boolean {
   // Your code goes here
 
   return true
+}
+
+private fun factor(number: Int): Collection<Int> {
+  val primeFactors = ArrayList<Int>()
+
+  // Your code goes here
+
+  return primeFactors
 }
 
 fun main(args: Array<String>) {
@@ -23,4 +34,16 @@ fun main(args: Array<String>) {
 
     index++
   }
+
+  println("Factoring large numbers...")
+  var totalTime = BigInteger.ZERO
+  for (i in Int.MAX_VALUE downTo Int.MAX_VALUE - 10000) {
+    val startTime = System.nanoTime()
+    val factors = factor(i)
+    totalTime = totalTime.add(BigInteger.valueOf(System.nanoTime().minus(startTime)))
+    if (factors.size.equals(2) || factors.size > 20)
+      println(factors)
+  }
+
+  println("Average time: " + totalTime.divide(BigInteger.valueOf(10000)) + "ns")
 }
