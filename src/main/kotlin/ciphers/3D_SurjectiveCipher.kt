d14e78c2856c63baf5dd35c86910a1d0d56d764d
breandan <breandan.considine@mail.mcgill.ca>
2021-09-19 16:58:43 -0400

    flatten the histogram to inhibit frequency analysis

--- src/main/kotlin/ciphers/3D_SurjectiveCipher.kt
+++ src/main/kotlin/ciphers/3D_SurjectiveCipher.kt
@@ -3,25 +3,30 @@ package ciphers
 import kotlin.random.Random
 
 fun main() {
-  val random = Random(1)
+  val random: Random = Random(1)
 
   val plaintext = "four score and seven years ago our fathers brought forth to this continent a new nation"
 
-  val codomain = (('a'..'z').toSet() + ('0'..'9').toSet()).shuffled(random)
+  // Make this set larger to increase the difficulty of cryptanalysis
+  val codomain = (('A'..'Z').toSet() + ('0'..'9').toSet()).shuffled(random).toMutableList()
 
   val mostFrequentChars = plaintext.lowercase().filter { it.isLetter() }.groupingBy { it }
-    .eachCount().entries.sortedByDescending { it.value }
+    .eachCount().entries.sortedByDescending { it.value }.map { (k, v) -> k to v.toDouble() / plaintext.length.toDouble() }.toMap()
 
   println("Plaintext histogram (size=${mostFrequentChars.size}): $mostFrequentChars")
 
-  val pairs = codomain.mapIndexed { i, c -> mostFrequentChars[i % mostFrequentChars.size].key to c }
-
   val key = HashMap<Char, MutableSet<Char>>()
-  pairs.forEach { (a, b) -> key.getOrPut(a) { mutableSetOf() }.add(b) }
+
+  mostFrequentChars.forEach { (t, _) -> key[t] = mutableSetOf(codomain.removeAt(0)) }
+
+  while(codomain.isNotEmpty())
+    key[mostFrequentChars.maxByOrNull { it.value / key[it.key]!!.size }!!.key]!!.add(codomain.removeAt(0))
+
   println("Key" + key.entries)
 
   val ciphertext = plaintext.lowercase().map { key[it]?.random(random) ?: it }.joinToString("")
 
+  // We want this histogram to be as flat as possible to inhibit frequency analysis
   println("Ciphertext histogram: " + ciphertext.groupingBy { it }.eachCount().entries.sortedByDescending { it.value })
 
   println("Ciphertext: $ciphertext")
